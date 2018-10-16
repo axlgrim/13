049e8facf377a68a61c5ecc450c51714f2df539e
Aliaksandr Shasholko <axl_grim@Aliaksandrs-MacBook-Pro-2.local>
2018-10-16 21:15:11 +0300

    New scenes
    Added Main Menu and Main Scene

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public void OnPlayClicked()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void OnQuitClicked()
+    {
+        Application.Quit();
+    }
+}
